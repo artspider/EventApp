2dcdd954a27c1382b9927255337129d0f4b957db
Arturo rodriguez <arturo.itiguala@gmail.com>
2022-04-27 11:43:36 -0500

    cambie migraciones2

--- database/migrations/2022_04_23_201156_create_event_itialumn_table.php
+++ database/migrations/2022_04_23_201156_create_event_itialumn_table.php
@@ -14,7 +14,6 @@ class CreateEventItialumnTable extends Migration
     public function up()
     {
         Schema::create('event_itialumn', function (Blueprint $table) {
-            $table->id();
             $table->primary(['itialumn_id','event_id']);
             $table->foreignId('itialumn_id')->constrained()->onDelete('cascade');
             $table->foreignId('event_id')->constrained()->onDelete('cascade');
